50f879cc4ac6f7f0dedcf4b0c2a43bf24933fd78
Lee Wise <theleewise@gmail.com>
2016-01-12 02:04:53 -0600

    Added wrapper and event actions for text and select

--- src/addformfields.js
+++ src/addformfields.js
@@ -27,9 +27,9 @@ function addFields(items,opts){
 
 
 
-function addSelect(opts){
+function addSelect(item){
 	try {
-		if (!Array.isArray(opts.val) && typeof opts.val !== 'object') throw "Values for select need to be in an object or array";
+		if (!Array.isArray(item.val) && typeof item.val !== 'object') throw "Values for select need to be in an object or array";
 	}
 	catch(err){
 		console.log(err);
@@ -38,18 +38,21 @@ function addSelect(opts){
 	var field = '<select>';
 
 	// If values are in array
-	if (Array.isArray(opts.val)){
-		for (var i = 0; i<opts.val.length; i++){
-			field += '<option>'+opts.val[i]+'</option>';
+	if (Array.isArray(item.val)){
+		for (var i = 0; i<item.val.length; i++){
+			field += '<option>'+item.val[i]+'</option>';
 		}
 	} else {
-		for (x in opts.val){
-			field += '<option value="'+opts.val[x]+'">'+x+'</option>';	
+		for (x in item.val){
+			field += '<option value="'+item.val[x]+'">'+x+'</option>';	
 		}
 	}
 
 
 	field += '</select>';
+	field = $(field);
+	if(item.fireevent){ field.on(item.fireevent.trigger,item.fireevent.action); }
+	if (item.wrapper){ field = field.wrapAll(item.wrapper).parent(); }
 	return field;
 }
 
@@ -65,9 +68,12 @@ function addRadio(item){
 		console.log(err);
 	}
 
-	function createRadio(selVal){
+	function createRadio(selVal,label,wrapper){
+		label = (label !== undefined) ? label : true;
 		var selected = ( item.selected === selVal) ? ' checked="checked"' : '';
-		return '<input '+cssClass+name+selected+'" type="radio" val="'+item.val[i]+'" />'
+		var field = '<input '+cssClass+name+selected+'" type="radio" val="'+selVal+'" />';
+		if (label){ field += '<label>'+selVal+'</label>'; }
+		return field;
 	}
 	
 	if ( item.selected && typeof item.selected !== 'string' && typeof item.selected !== 'number' ){
@@ -81,15 +87,15 @@ function addRadio(item){
 	// If values are in array
 	if (Array.isArray(item.val)){
 		for (var i = 0; i<item.val.length; i++){
-			field += createRadio(i);
+			field += createRadio(item.val[i],item.label,item.wrapperEach);
 		}
 	} else {
 		for (x in item.val){
-			field += createRadio(x);
-			field += '<label>'+x+'</label>';	
+			field += createRadio(x,item.label,item.wrapperEach);
 		}
 	}
 
+	if (item.wrapper){ field = $(item.wrapper).html(field); }
 	return field;
 }
 
@@ -102,5 +108,8 @@ function addText(item){
 	var id = (item.id) ? ' id="'+item.id+'"' : '';
 	var placeholder = (item.placeholder) ? ' placeholder="'+item.placeholder+'"' : '';
 	var field = '<input'+cssClass+id+placeholder+' type="text" value="testing" />';
+	field = $(field);
+	if(item.fireevent){ field.on(item.fireevent.trigger,item.fireevent.action); }
+	if (item.wrapper){ field = field.wrapAll(item.wrapper).parent(); }
 	return field;
 }
