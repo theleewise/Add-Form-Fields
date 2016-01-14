f0394d9bc62fec1aed3ce193bd9eeb465f98f0ad
Lee Wise <theleewise@gmail.com>
2016-01-13 23:50:36 -0600

    Add options for checkboxes & textarea

--- src/addformfields.js
+++ src/addformfields.js
@@ -1,3 +1,13 @@
+/* THINGS TO ADD 
+	* Autofocus option to all
+	* Accessability option to all
+	* Textarea
+	* Submit
+	* Reset
+	* Password
+	* Checkboxes
+*/
+
 var count = 1;
 function addFields(items,opts){
 	if(!opts || !opts.max || count <= opts.max){
@@ -10,13 +20,25 @@ function addFields(items,opts){
 					var field = addText(items[i]);
 					break;
 				case 'radio':
-					var field = addRadio(items[i]);
+					var field = addRadioCheckbox(items[i]);
+					break;
+				case 'checkbox':
+					var field = addRadioCheckbox(items[i]);
+					break;
+				case 'button':
+					var field = addButton(items[i]);
+					break;
+				case 'submit':
+					var field = addSubmit(items[i]);
+					break;
+				case 'textarea':
+					var field = addTextArea(items[i]);
 					break;
 				default:
 					var field = '<input type="text" />';
 			}
 
-			$(items[i].target).append(field);
+			$(opts.target).append(field);
 		}
 		if(opts && opts.max && opts.maxFunc && count >= opts.max){ opts.maxFunc(); }
 		count++;
@@ -35,7 +57,14 @@ function addSelect(item){
 		console.log(err);
 	}
 	
-	var field = '<select>';
+	var cssClass = (item.cssClass) ? ' class="'+item.cssClass+'"' : '';
+	var id = (item.id) ? ' id="'+item.id+'"' : '';
+	var label = '';
+	if(item.label){
+		var labelFor = (item.id) ? ' for="'+item.id+'"' : '';
+		var label = '<label'+labelFor+'>'+item.label+'</label>';
+	}
+	var field = '<select'+cssClass+'">';
 
 	// If values are in array
 	if (Array.isArray(item.val)){
@@ -50,7 +79,7 @@ function addSelect(item){
 
 
 	field += '</select>';
-	field = $(field);
+	field = $(label+field);
 	if(item.fireevent){ field.on(item.fireevent.trigger,item.fireevent.action); }
 	if (item.wrapper){ field = field.wrapAll(item.wrapper).parent(); }
 	return field;
@@ -60,7 +89,7 @@ function addSelect(item){
 
 
 
-function addRadio(item){
+function addRadioCheckbox(item){
 	try {
 		if (!Array.isArray(item.val) && typeof item.val !== 'object') throw "Values for radio buttons need to be in an object or array";
 	}
@@ -68,30 +97,46 @@ function addRadio(item){
 		console.log(err);
 	}
 
-	function createRadio(selVal,label,wrapper){
-		label = (label !== undefined) ? label : true;
-		var selected = ( item.selected === selVal) ? ' checked="checked"' : '';
-		var field = '<input '+cssClass+name+selected+'" type="radio" val="'+selVal+'" />';
-		if (label){ field += '<label>'+selVal+'</label>'; }
+	function createItem(selVal,selected,wrapper){
+
+		if (typeof selVal === 'string'){
+			var val = selVal;
+			var label = selVal;
+			if (typeof selected === 'string'){
+				var checked = ( selected === selVal ) ? ' checked' : '';
+			} else if (Array.isArray(selected)) {
+				var checked = ( selected.indexOf(selVal) >= 0 ) ? ' checked' : '';
+			}
+		}
+
+		else if (typeof selVal === 'object'){
+			var val = selVal[0];
+			var label = selVal[1];
+			if (typeof selected === 'string'){
+				var checked = ( item.selected === selVal[0]) ? ' checked' : '';
+			} else if (Array.isArray(selected)){
+				var checked = ( selected.indexOf(selVal[0]) >= 0 ) ? ' checked' : '';
+			}
+		}
+
+		var field = '<label><input '+cssClass+name+checked+' type="'+item.type+'" val="'+val+'" />'+label+'</label>';
 		return field;
 	}
 	
-	if ( item.selected && typeof item.selected !== 'string' && typeof item.selected !== 'number' ){
-		console.log('Selected radio value must be a string or number')
-	}
 	var cssClass = (item.cssClass) ? ' class="'+item.cssClass+'"' : '';
 	var id = (item.id) ? ' id="'+item.id+'"' : '';
 	var name = (item.name) ? ' name="'+item.name+'"' : '';
 
 	var field = '';
+
 	// If values are in array
 	if (Array.isArray(item.val)){
 		for (var i = 0; i<item.val.length; i++){
-			field += createRadio(item.val[i],item.label,item.wrapperEach);
+			field += createItem(item.val[i],item.selected,item.wrapperEach);
 		}
 	} else {
 		for (x in item.val){
-			field += createRadio(x,item.label,item.wrapperEach);
+			field += createItem([x,item.val[x]],item.selected,item.wrapperEach);
 		}
 	}
 
@@ -107,7 +152,62 @@ function addText(item){
 	var cssClass = (item.cssClass) ? ' class="'+item.cssClass+'"' : '';
 	var id = (item.id) ? ' id="'+item.id+'"' : '';
 	var placeholder = (item.placeholder) ? ' placeholder="'+item.placeholder+'"' : '';
-	var field = '<input'+cssClass+id+placeholder+' type="text" value="testing" />';
+	var focus = (item.focus) ? ' autofocus' : '';
+	var label = '';
+	if(item.label){
+		var labelFor = (item.id) ? ' for="'+item.id+'"' : '';
+		var label = '<label'+labelFor+'>'+item.label+'</label>';
+	}
+	var field = '<input type="text"'+cssClass+id+placeholder+focus+' value="'+item.val+'" />';
+	field = $(label+field);
+	if(item.fireevent){ field.on(item.fireevent.trigger,item.fireevent.action); }
+	if (item.wrapper){ field = field.wrapAll(item.wrapper).parent(); }
+	return field;
+}
+
+
+
+
+
+function addTextArea(item){
+	var cssClass = (item.cssClass) ? ' class="'+item.cssClass+'"' : '';
+	var id = (item.id) ? ' id="'+item.id+'"' : '';
+	var placeholder = (item.placeholder) ? ' placeholder="'+item.placeholder+'"' : '';
+	var focus = (item.focus) ? ' autofocus' : '';
+	var label = '';
+	if(item.label){
+		var labelFor = (item.id) ? ' for="'+item.id+'"' : '';
+		var label = '<label'+labelFor+'>'+item.label+'</label>';
+	}
+	var field = '<textarea'+cssClass+id+placeholder+focus+'>'+item.val+'</textarea>';
+	field = $(label+field);
+	if(item.fireevent){ field.on(item.fireevent.trigger,item.fireevent.action); }
+	if (item.wrapper){ field = field.wrapAll(item.wrapper).parent(); }
+	return field;
+}
+
+
+
+
+
+function addButton(item){
+	var cssClass = (item.cssClass) ? ' class="'+item.cssClass+'"' : '';
+	var id = (item.id) ? ' id="'+item.id+'"' : '';
+	var field = '<button type="button"'+cssClass+id+'>'+item.val+'</button>';
+	field = $(field);
+	if(item.fireevent){ field.on(item.fireevent.trigger,item.fireevent.action); }
+	if (item.wrapper){ field = field.wrapAll(item.wrapper).parent(); }
+	return field;
+}
+
+
+
+
+
+function addSubmit(item){
+	var cssClass = (item.cssClass) ? ' class="'+item.cssClass+'"' : '';
+	var id = (item.id) ? ' id="'+item.id+'"' : '';
+	var field = '<button type="submit"'+cssClass+id+'>'+item.val+'</button>';
 	field = $(field);
 	if(item.fireevent){ field.on(item.fireevent.trigger,item.fireevent.action); }
 	if (item.wrapper){ field = field.wrapAll(item.wrapper).parent(); }
